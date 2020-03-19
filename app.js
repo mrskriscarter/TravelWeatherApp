736233af88fe29f940cb8fd2e36fb47af058a219
Kris Carter <mrskristacarter@gmail.com>
2020-03-19 11:32:31 -0600

    working on date

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const hereKey = "Sqppyp13Y687AczP7Aw-2qZMXksmAz6isqVUFbq-wxo"
+
 async function GetLocations(startLocation, endLocation) {
     startLocation = encodeURIComponent(startLocation);
     endLocation = encodeURIComponent(endLocation);
@@ -11,7 +13,7 @@ async function GetLocations(startLocation, endLocation) {
     var routePointsJson = await routePoints.json();
     //console.log(routePointsJson);
     var gpsCoordinates = routePointsJson.route.shape.shapePoints;
-    console.log(gpsCoordinates);
+    //console.log(gpsCoordinates);
     var processedPoints = [];
     while (gpsCoordinates.length > 0) {
         let nextTuple = gpsCoordinates.splice(0,2);
@@ -34,28 +36,59 @@ async function GetCheckPoints(processedPoints, checkpoints) {
 };
 
     async function GetGeo(latitude, longitude) {
-        let url = await fetch(`https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=${latitude}%2C${longitude}%2C3000&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=Sqppyp13Y687AczP7Aw-2qZMXksmAz6isqVUFbq-wxo`);
+        let url = await fetch(`https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?prox=${latitude}%2C${longitude}%2C3000&mode=retrieveAddresses&maxresults=1&gen=9&apiKey=${hereKey}`);
         let jsonResponse = await url.json();
         let address = jsonResponse.Response.View[0].Result[0].Location.Address;
         return {'city': address.City, 'state': address.State}
     };
+
+    async function getWeatherForecastForLocation(latitude, longitude) {
+        let forecast =`https://weather.ls.hereapi.com/weather/1.0/report.json?product=forecast_hourly&latitude=${latitude}&longitude=${longitude}&oneobservation=true&apiKey=${hereKey}&metric=false`
+        let forcastResponse = await fetch(forecast);
+        let forcastJsonRespnse = await forcastResponse.json();
+
+        return forcastJsonRespnse.hourlyForecasts.forecastLocation.forecast
+        
+
+    };
+
+    function DateToIsoDate(date) {
+        return date.toISOString().split("T")[0]
+    };
+
+    Date.prototype.addDays = function(days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+    }
+
+    // async function loopThurDates() {
+    //     let inputDate = userDate;
+    //     let hereDate = `https://weather.ls.hereapi.com/weather/1.0/report.json?product=forecast_7days&latitude=${latitude}&longitude=${longitude}&oneobservation=true&apiKey=${hereKey}&metric=false`
+    //     let hereDateResponse = await fetch(hereDate);
+    //     let hereDateJsonResponse = await hereDateResponse.json();
+
+
+    // }
+ 
 
 
 
 var app = new Vue({
     el: '#app',
     data: {
-        //ishidden: false,
 
         startLocation: "St George",
         endLocation: "Logan",
         checkpoints: 0,
         filteredlocations: [],
+        dayoftravel: DateToIsoDate(new Date()),
+        minDateOfTravel: DateToIsoDate(new Date()),
+        maxDateOfTravel: DateToIsoDate(new Date().addDays(15)),
         isSecondPage: false,
         isThirdPage: false,
         isMainPage: true,
       
-        testdata: "Vue is working",
  
     },
 
@@ -63,16 +96,20 @@ var app = new Vue({
 
         getLocations: async function() {
             console.log(this.checkpoints);
+            console.log(this.dayoftravel)
             var locations = await GetLocations(this.startLocation, this.endLocation);
             var filteredlocations = await GetCheckPoints(locations, this.checkpoints);
             //this.filteredlocations = filteredlocations;
             for (index in filteredlocations) {
                 //console.log(filteredlocations[index]);
                 filteredlocations[index].name = await GetGeo(filteredlocations[index].coordinate.latitude, filteredlocations[index].coordinate.longitude);
+                //filteredlocations[index].forcast = await getWeatherForecastForLocation(filteredlocations[index].coordinate.latitude, filteredlocations[index].coordinate.longitude);
                 
             }
             this.filteredlocations = filteredlocations;
-            console.log("filtered", filteredlocations)
+            console.log("filtered", filteredlocations);
+    
+
 
             //console.log(filteredlocations[1].name.city)
             //var gps = await GetGeo(locations[0].latitude, locations[0].longitude);
@@ -85,7 +122,7 @@ var app = new Vue({
         },
         backButton1: function () {
                 this.SecondPage = false,
-                this.isMainPage = true
+                this.isMainPage = true,
                 this.isThirdPage = false
         },
 
@@ -94,8 +131,15 @@ var app = new Vue({
             this.SecondPage = true,
             this.isMainPage = false
     },
-        moreInfo: function () {
+        formatDate: function (userDate) {
+            return moment(userDate).format();
 
+        },
+
+        moreInfo: function () {
+            this.isMainPage = false,
+            this.SecondPage = false,
+            this.isThirdPage = true
         }
 
     },
